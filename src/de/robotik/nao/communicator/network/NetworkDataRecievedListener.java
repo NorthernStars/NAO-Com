690bd3ec585031d06728390cf5b534c3fffd478b
hannes eilers <hannes@sallysue-thinkpad>
2014-03-05 18:47:15 +0100

    Restuctured project to fix eclipse import errors.

--- src/de/robotik/nao/communicator/network/NetworkDataRecievedListener.java
+++
@@ -1,7 +0,0 @@
-package de.robotik.nao.communicator.network;
-
-public interface NetworkDataRecievedListener {
-
-	public void onNetworkDataRecieved(String data);
-	
-}
--- src/de/robotik/nao/communicator/network/data/request/RequestType.java
+++
@@ -1,9 +0,0 @@
-package de.robotik.nao.communicator.network.data.request;
-
-public enum RequestType {
-
-	REQUEST,
-	SET,
-	SKIP;
-	
-}
