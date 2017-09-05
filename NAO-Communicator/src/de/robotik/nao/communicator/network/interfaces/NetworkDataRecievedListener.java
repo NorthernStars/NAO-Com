eda360bc2e77a554e12b0baead22cba1b59c7373
Hannes Eilers <mail@hanneseilers.de>
2017-09-05 18:25:34 +0200

    Deleted old app files. Leave only holo color picker for led color picker widget.

--- NAO-Communicator/src/de/robotik/nao/communicator/network/interfaces/NetworkDataRecievedListener.java
+++
@@ -1,9 +0,0 @@
-package de.robotik.nao.communicator.network.interfaces;
-
-import de.robotik.nao.communicator.network.data.response.DataResponsePackage;
-
-public interface NetworkDataRecievedListener {
-
-	public void onNetworkDataRecieved(DataResponsePackage data);
-	
-}
